618faea95bdd5fee1a32a6dc43eb01d82e83ebe7
zlenkoarty <95541599+zlenkoarty@users.noreply.github.com>
2023-07-26 02:26:26 +0300

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,15 +1,30 @@
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_table
 from dash.dependencies import Input, Output
 import pandas as pd
 
-df = pd.read_excel('turnover.xlsx')
+df = pd.read_excel('текучесть.xlsx')
 pivot_table = df.pivot_table(
     values=['Общая текучесть','Текучесть 3 месяца', 'Текучесть 6 месяца', 'Текучесть 12 месяца'],
     index=['Организация', 'Группа подразделений', 'Группа должностей', 'Пол'],
     aggfunc='mean'
 ) * 100
+
+# Round and format DataFrame for the table
+formatted_table = pivot_table.reset_index()
+for col in ['Общая текучесть','Текучесть 3 месяца', 'Текучесть 6 месяца', 'Текучесть 12 месяца']:
+    formatted_table[col] = formatted_table[col].round(2).apply(lambda x: f'{x}%')
+
+# Define column order
+column_order = ['Организация', 'Группа подразделений', 'Группа должностей', 'Пол', 'Общая текучесть', 'Текучесть 3 месяца', 'Текучесть 6 месяца', 'Текучесть 12 месяца']
+
+# Reorder columns
+formatted_table = formatted_table.reindex(columns=column_order)
+
+# Write DataFrame to an excel
+formatted_table.to_excel('formatted_table.xlsx', index=False)
 
 app = dash.Dash(__name__, external_stylesheets=['https://codepen.io/chriddyp/pen/bWLwgP.css'])
 server = app.server
@@ -33,7 +48,51 @@ app.layout = html.Div([
     html.Div(children=[
         html.Div(dcc.Graph(id='6m-turnover-graph'), className="six columns"),
         html.Div(dcc.Graph(id='12m-turnover-graph'), className="six columns"),
-    ], className="row")
+    ], className="row"),
+
+    html.Hr(),  # Horizontal line
+
+    #html.A('Скачать данные', href='/download_excel/', download="formatted_table.xlsx", id='download-link'),
+
+   
+    html.Div(dcc.Markdown('''
+    **Анализ текучести кадров**
+
+    1. **Общая текучесть:**
+       - Общая текучесть для всей организации "ООО ПЕТРОВИЧ-ТЕХ" составляет 0,0%. Это означает, что за наблюдаемый период сотрудники не покидали компанию.
+
+    2. **Текучесть по полу:**
+       - Во всех подразделениях и группах должностей текучесть как для мужчин, так и для женщин относительно низкая и составляет 0,0%. Это говорит о том, что компания успешно удерживает сотрудников обоих полов.
+
+    3. **Текучесть по подразделениям:**
+       - В подразделении "ГО" текучесть для мужчин составляет 20,0%, что является наивысшим показателем среди всех подразделений. Однако для женщин в этом подразделении оттока нет.
+
+    4. **Текучесть по группам должностей:**
+       - Для группы должностей "Грузчик" в подразделении "РЦ Пискаревский" наблюдается наивысшая текучесть среди всех должностей и составляет 46,15% за 12 месяцев. Это указывает на возможные проблемы с удержанием сотрудников на этой должности.
+
+    5. **Текучесть по подразделениям и полу:**
+       - В подразделении "КЦ" текучесть как для мужчин (35,97%), так и для женщин (31,74%) относительно высокая по сравнению с другими подразделениями.
+
+    6. **Текучесть по подразделениям и группам должностей:**
+       - В подразделении "РЦ Новосаратовка" наблюдается значительная текучесть в различных группах должностей, особенно для мужчин на должностях "Кладовщики площадка" (22,22%) и "Комплектовщики САХ" (36,07%).
+
+    В целом, компания имеет низкую текучесть, что является положительным сигналом и указывает на стабильность трудового коллектива. Однако есть конкретные подразделения и должности, где текучесть относительно выше, особенно среди мужчин. Для разработки соответствующих стратегий удержания необходимо провести дополнительный анализ и исследование причин текучести в этих областях. Следует отметить, что показатели текучести могут меняться со временем, поэтому непрерывный мониторинг и анализ являются важными для решения потенциальных проблем удержания сотрудников.
+    
+    Ниже представленна таблица, лежащей в основе построяния графиков. Скачать таблицу можно, нажав кнопку "EXPORT"
+    '''), className="row"),
+
+    html.Hr(),
+    html.Div(
+        dash_table.DataTable(
+            id='table',
+            columns=[{"name": i, "id": i} for i in formatted_table.columns],
+            data=formatted_table.to_dict('records'),
+            export_format="xlsx"
+        ),
+        className="row"
+    ),
+    html.Hr(),
+    
 
 ], className="ten columns offset-by-one")
 
@@ -81,4 +140,4 @@ def update_graphs(selected_filter):
     return graphs[0], graphs[1], graphs[2], graphs[3]
 
 if __name__ == '__main__':
-    app.run_server(debug=False)
+    app.run_server(debug=True)
